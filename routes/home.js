1b889b992f3bd8e05d628c57872391671ee9decb
Unknown <w_zhaoming@hotmail.com>
2017-10-15 13:36:16 +1300

    ZM route.js

--- routes/home.js
+++ routes/home.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 var homeController = require('../src/controllers/home');
-var searchController = require('../src/searchController/home');
+var searchController = require('../src/controllers/search');
 
 /* GET home page. */
 router.get('/', homeController.index);
-router.get('/search',searchController);
+//router.get('/search',searchController);
 module.exports = router;
